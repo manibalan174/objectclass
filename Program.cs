33bc739cef64e1da1c18caaa509133876c233cd0
manibalan174 <45997825+manibalan174@users.noreply.github.com>
2021-05-11 16:01:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace StaticFunction1
+{
+     class Program
+    {
+
+        public   enum  Seson  {
+            Winter=10,
+            Spring,
+            Summer,
+            Atoumn,
+            Fall
+        };
+         public enum fruits { Apple , Orange, Moango, JackFruits };
+        public static void EnumTraversNames()
+        {
+               foreach (string frt in Enum.GetNames(typeof(fruits)))  
+                 {  
+                     Console.WriteLine(frt);  
+                 } 
+
+        }
+         public static void EnumTraversValues()
+        {
+               foreach (fruits frt in Enum.GetValues(typeof(fruits)))  
+                 {  
+                     Console.WriteLine(frt);  
+                 } 
+
+        }
+           
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int Sesons1 =(int) Seson.Winter;
+            int Sesons2 =(int) Seson.Spring;
+            int Sesons3 = (int)Seson.Summer;
+            Console.WriteLine("Sesond  = {0} \n Sesons = {1} \n Sesons = {2}",Sesons1,Sesons2 ,Sesons3 );
+             EnumTraversNames();
+             EnumTraversValues();
+
+        }
+    }
+
+}
---
+++ structClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StructConcept
+{
+
+
+public struct Sum
+{
+     int Sum1;
+     int Sum2;
+     public  Sum(int Sum1, int Sum2)
+     {
+         this.Sum1 = Sum1;
+         this.Sum2 = Sum2;
+          
+     }
+     public void display()
+     {
+         int Add = Sum1 + Sum2;
+         Console.WriteLine("Add VAlues : "+Add);
+     }
+     static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Sum S = new Sum(12,13);
+            //S.Sum(12,13);
+            S.display();
+        }
+}
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Console.WriteLine("Hello World!");
+    //     }
+    // }
+}
